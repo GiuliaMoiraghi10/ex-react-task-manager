645182e1dc2480aeef37ce9b39033236a85a7b28
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-03-29 20:08:03 +0100

    Milestone 3 + style

---
+++ src/components/TaskRow.jsx
@@ -0,0 +1,23 @@
+/* Questo componente rappresenta una sola riga della tabella
+mostra le proprietà: title, status e createdAt (no description)
+*/
+
+import { memo } from 'react'
+
+const TaskRow = memo(({ task }) => {
+
+    // creo variabile x impostare classi colori allo status
+    const statusClass = task.status.replace(" ", "").toLowerCase()
+
+    return (
+        <tr>
+            <td>{task.title}</td>
+            <td className={statusClass}>{task.status}</td>
+            <td>{new Date(task.createdAt).toLocaleDateString()}</td>
+        </tr>
+    )
+})
+
+export default TaskRow
+
+
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,7 +1,34 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext'
+import TaskRow from '../components/TaskRow'
+
 export default function TaskList() {
+
+    // prendo task da GlobalContext
+    const { tasks } = useContext(GlobalContext)
+    console.log(tasks)
+
     return (
-        <div className="flex justify-center py-10">
-            <h2 className="font-semibold text-xl">Elenco Task</h2>
-        </div>
+        <>
+            <h2 className="font-semibold text-xl text-center mb-6 mt-10">Elenco Task</h2>
+            <div className="flex justify-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Stato</th>
+                            <th>Data creazione</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tasks &&
+                            tasks.map((task) => {
+                                return <TaskRow key={task.id} task={task} />
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
