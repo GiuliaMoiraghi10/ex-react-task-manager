77b0ab7b847d1d400f11972100c1ef75b142a781
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-03-31 14:28:08 +0200

    Milestone 5 - form funzionante in console - useMemo + useRef

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,4 +1,7 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useMemo } from 'react'
+
+// variabile con caratteri vietati per il titolo
+const symbols = '"!@#$%^&*()-_=+[]{}|;:/,.<>?/`~"'
 
 export default function AddTask() {
 
@@ -7,10 +10,37 @@ export default function AddTask() {
     const descriptionRef = useRef()
     const statusRef = useRef()
 
+    // funzione per gestire gli errori
+    // faccio useMemo per evitare stato aggiuntivo nell'useEffect
+    // useMemo ricalcola ogni volta che cambia taskTitle
+    const taskTitleError = useMemo(() => {
+        if (!taskTitle.trim()) // se ritorna una stringa vuota
+            return 'Il titolo della Task non può essere vuoto'
+        if ([...taskTitle].some(char => symbols.includes(char))) // se il titolo contiene almeno uno dei caratteri speciali
+            return 'Il titolo della Task non può contenere caratteri speciali'
+        return '' // se non ci sono errori ritorna stringa vuota
+    }, [taskTitle])
+
+    // handleSubmit x evitare refresh del form
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        // variabile per aggiungere nuova task dal form
+        const newTask = {
+            title: taskTitle.trim(),
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        }
+
+        // stampo in console la nuova task
+        console.log('Nuova Task da aggiungere:', newTask)
+    }
+
     return (
         <div>
             <h2 className="font-semibold text-xl text-center mb-6 mt-10">Aggiungi Task</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
+                {/* Nome Task */}
                 <label>
                     Nome Task:
                     <input
@@ -19,10 +49,16 @@ export default function AddTask() {
                         onChange={e => setTaskTitle(e.target.value)}
                     />
                 </label>
+                {/* se c'è un errore, lo mostro sotto l'input in rosso */}
+                {taskTitleError && <p className='text-red-500 mb-3'>{taskTitleError}</p>}
+
+                {/* Descrizione Task */}
                 <label>
                     Descrizione:
                     <textarea ref={descriptionRef}></textarea>
                 </label>
+
+                {/* Stato Task */}
                 <label>
                     Stato:
                     <select ref={statusRef}>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function NotFound() {
     return (
         <section className="min-h-full min-w-full">
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center p-20">
                 <h2 className="text-2xl">404</h2>
                 <strong>Pagina non trovata</strong>
+                <Link to='/tasks' className="text-white bg-pink-300 p-2 mt-10 rounded-md">Torna alla home</Link>
             </div>
         </section>
     )
